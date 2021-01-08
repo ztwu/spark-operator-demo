fcfe3e927d0197faa2c23f9883eeba8b1a2dc5e1
ztwu4 <ztwu4@iflytek.com>
2021-01-08 16:31:24 +0800

    uh

--- src/main/java/com/iflytek/edcc/TextSizeTest.java
+++ src/main/java/com/iflytek/edcc/TextSizeTest.java
@@ -8,6 +8,23 @@ public class TextSizeTest {
             System.out.println("旭日东升12acb".getBytes().length);
             System.out.println("旭日东升12acb".getBytes("GB2312").length);
             System.out.println("旭日东升12acb".getBytes("UTF-8").length);
+
+            String s = "旭";
+            char[] cs = s.toCharArray();
+            byte[] bs = s.getBytes("GBK");
+//            byte[] bs = s.getBytes();
+//            for(char ci : cs){
+//                System.out.println(ci);
+//                System.out.println((int)ci);
+//            }
+            System.out.println("-------------------------");
+            for(byte bi:bs){
+                System.out.println(bi); //10
+                System.out.println(Integer.toBinaryString(bi)); //2
+                System.out.println(Integer.toHexString(bi)); //16
+                System.out.println(Integer.toOctalString(bi)); //8
+            }
+
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
