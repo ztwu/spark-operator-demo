79a7bc067c20d884a14551f3a5421265ef08ed58
ztwu4 <ztwu4@iflytek.com>
2021-01-20 17:12:47 +0800

    uh

--- src/main/scala/com/iflytek/edcc/dataframe/CompressDemo.scala
+++ src/main/scala/com/iflytek/edcc/dataframe/CompressDemo.scala
@@ -2,6 +2,7 @@ package com.iflytek.edcc.dataframe
 
 import org.apache.hadoop.io.SequenceFile
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions._
 
 object CompressDemo {
   def main(args: Array[String]): Unit = {
@@ -11,6 +12,7 @@ object CompressDemo {
       .appName("spark-job")
       .getOrCreate
     val conf = spark.conf
+    import spark.implicits._
 
     // text compress
     conf.set("mapreduce.output.fileoutputformat.compress", "true")
@@ -24,6 +26,20 @@ object CompressDemo {
       ("hong", 19, 13287994007L),
       ("zhi", 21, 15552211523L)
     )) toDF("name", "age", "phone")
+
+    val cols = data.columns
+    var data2 = data
+    for(col<-cols){
+      data2 = data2.withColumn(s"new_${col}", concat_ws(":", lit(s"${col}"), data(s"${col}")))
+    }
+    data2 = data2.withColumn("array", concat_ws(",",col("new_name"),
+      col("new_age"), col("new_phone")))
+        .withColumn("split", split(col("array"),","))
+        .select(col("name"), explode(col("split")) as "values")
+        .withColumn("valuearray", split(col("values"),":"))
+        .withColumn("key", col("valuearray").getItem(0))
+        .withColumn("value", col("valuearray").getItem(1))
+    data2.show()
 
     data.select("name")
     .write
